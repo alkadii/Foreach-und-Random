7d687efab7e44e9a8cb0be41ba358f44a642bbd0
khaled <alkadi.khaled@hotmail.com>
2021-10-25 08:58:26 +0200

    Foreach 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = " Khaled Alkadi";
+            char[] c = str.ToCharArray();
+            foreach(char d in str)
+            {
+                Console.Write(d+" ");
+            }
+
+
+        }
+    }
+}
