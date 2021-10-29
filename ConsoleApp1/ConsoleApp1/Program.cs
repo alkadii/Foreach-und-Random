c6c8ed17f937beba3b2f3b85a7974afc81f484b8
khaled <alkadi.khaled@hotmail.com>
2021-10-29 10:01:32 +0200

    21

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,19 +10,90 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            string str = " Khaled Alkadi";
-            char[] c = str.ToCharArray();
-            foreach (char d in str)
-            {
-                Console.Write(d);
+            //Foreach
+            #region foreach(V-Type V-Name in V(, dass wiederholt werden soll)) {.......}
 
-            }
+            //string str = " Khaled Alkadi";
+            //char[] c = str.ToCharArray();
+            //foreach (char d in str)
+            //{
+            //    Console.Write(d);
+
+            //}
 
             //int[] t = { 6, 5, 4, 3, 8, 9 };
             //foreach(int s in t)
             //{
             //    Console.Write(s+" "); 
             //}
+
+            //char[] chaName = new char[] { 'f', 'g', 'j', 'd', 'k', 'q' };
+            //Console.WriteLine(chaName);
+
+
+            #endregion
+
+            #region Random V-Name = new Random();
+            // 1:
+
+            //Random ran = new Random();
+            //int su = ran.Next();
+            //Console.WriteLine(su);
+
+            // 2:
+
+            //Random t = new Random();
+            //Console.WriteLine(t.Next());
+
+            // 3:
+
+            //Random re = new Random();
+            //int f = re.Next(50, 100);
+            //Console.WriteLine(f);
+            //Console.WriteLine(f);
+
+
+            //start:
+            //Console.Write(" Zahl eingeben : ");
+            //int t = int.Parse(Console.ReadLine());
+            //Random ran = new Random();
+            //int sum = ran.Next(0,t);
+
+            //Console.WriteLine(sum);
+            //goto start;
+
+
+            #endregion
+
+            #region Upper/Lower :  
+
+            // 1: 
+
+            //string str = "khaled";
+            //Console.WriteLine(str.ToUpper());
+
+            // 2:
+
+            //string[] name = new string[] { "H", "F", "S" };
+            //for (int x = 0; x < name.Length; x++)
+            //{
+            //    Console.WriteLine(name[x].ToLower());
+            //}
+
+
+            //string strNam = "khaled";
+            //char[] chnam = strNam.ToCharArray();
+            //foreach (char w in strNam.ToLower())
+            //{
+
+            //    Console.WriteLine(w);
+            //}
+
+
+            #endregion
+
+
+
 
 
         }
